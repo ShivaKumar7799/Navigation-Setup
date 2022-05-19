a68fff201c235b1390f2560996d974606c8bbf24
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2022-05-19 18:07:35 +0530

    final update

--- index.js
+++ index.js
@@ -123,73 +123,31 @@ inputKey.addEventListener("keydown", e => {
   }
 })
 
-// const shareData = {
-//   title: 'MDN',
-//   text: 'Learn web development on MDN!',
-//   url: 'https://developer.mozilla.org'
-//   // files : ["https://cdn.pixabay.com/photo/2022/05/12/19/11/flowers-7192179__480.jpg","https://cdn.pixabay.com/photo/2022/05/04/09/13/bordeaux-7173548__340.jpg"]
-// }
+const shareData = {
+  title: 'MDN',
+  text: 'Learn web development on MDN!',
+  url: 'https://developer.mozilla.org'
+  // files : ["https://cdn.pixabay.com/photo/2022/05/12/19/11/flowers-7192179__480.jpg","https://cdn.pixabay.com/photo/2022/05/04/09/13/bordeaux-7173548__340.jpg"]
+}
 
-// let shareBtn = document.getElementById("shareBtn")
+let shareBtn = document.getElementById("shareBtn")
 
-// shareBtn.addEventListener("click", function(){
-//   navigator.share(shareData). then( (resp) => console.log(resp)).catch((err) => console.log(err))
-// })
-
-// var shareButton = document.getElementById('share-button');
-
-// shareButton.addEventListener('click', function () {
-
-//     // Check if navigator.share is supported by the browser
-//     if (navigator.share) {
-//         console.log("Congrats! Your browser supports Web Share API")
-
-//         // navigator.share accepts objects which must have atleast title, text or
-//         // url. Any text or title or text is possible
-//         navigator.share({
-//                 title: "Bits and pieces: Web Share API article",
-//                 text: "Web Share API feature is awesome. You must check it",
-//                 url: window.location.href
-//             })
-//             .then(function () {
-//                 console.log("Shareing successfull")
-//             })
-//             .catch(function () {
-//                 console.log("Sharing failed")
-//             })
-
-//     } else {
-//         console.log("Sorry! Your browser does not support Web Share API")
-//     }
-// })
-// let filesArray = ["https://cdn.pixabay.com/photo/2022/05/12/19/11/flowers-7192179__480.jpg","https://cdn.pixabay.com/photo/2022/05/04/09/13/bordeaux-7173548__340.jpg"]
-
-// var shareData = { files: filesArray };
-
-// if (navigator.canShare && navigator.canShare(shareData)) {
-//   // Adding title afterwards as navigator.canShare just
-//   // takes files as input
-//   shareData.title = "Bits and pieces"
-  
-//   navigator.share(shareData)
-//   .then(() => console.log('Share was successful.'))
-//   .catch((error) => console.log('Sharing failed', error));
-  
-// } else {
-//   console.log("Your system doesn't support sharing files.");
-// }
+shareBtn.addEventListener("click", function(){
+  navigator.share(shareData). then( (resp) => console.log(resp)).catch((err) => console.log(err))
+})
 
 var shareButton = document.getElementById('share-button');
-let filesArray = ["https://cdn.pixabay.com/photo/2022/05/12/19/11/flowers-7192179__480.jpg","https://cdn.pixabay.com/photo/2022/05/04/09/13/bordeaux-7173548__340.jpg"]
+
 shareButton.addEventListener('click', function () {
-    // var filesArray = document.getElementById('share-files').files
+    var filesArray = document.getElementById('share-files').files;
+    console.log("files array", filesArray)
     var shareData = { files: filesArray };
 
     if (navigator.canShare && navigator.canShare(shareData)) {
 
     // Adding title afterwards as navigator.canShare just
     // takes files as input
-    shareData.title = "Bits and pieces"
+    shareData.title = "Sharing Images"
 
     navigator.share(shareData)
     .then(() => console.log('Share was successful.'))
